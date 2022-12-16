a226463fdeb15166200c7a80ab38ff97fb8f282e
coxxy <ezeiyokecosmas@gmail.com>
2022-12-16 14:56:47 +0000

    first commit

---
+++ encryption.py
@@ -0,0 +1,47 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input('Type "encode" to encrypt or "decode" to decrypt\n'  )
+text = input('Type your message\n').lower()
+shift = int(input('Type the shift number :\n'))
+ 
+ 
+def encrypt(plain_text, shift_amount):
+    cipher_text = " "
+    for letter in plain_text :
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+        
+    print(f'The encoded text is {cipher_text}')
+     
+     
+     
+# encrypt(plain_text = text, shift_amount = shift )
+
+
+
+def decrypt (cipher_text, shift_amount) :
+    cipher_decrypt = " "
+    for letter in cipher_text :
+        position = alphabet.index(letter)
+        new_position = position - shift_amount 
+        new_letter = alphabet[new_position]
+        cipher_decrypt += new_letter
+
+    print(f'The encoded text is {cipher_decrypt}')
+
+
+# decrypt(cipher_text = text, shift_amount = shift )
+
+
+if direction == "encode" :
+    encrypt(plain_text = text, shift_amount = shift)
+
+
+elif direction == "decode" :
+    decrypt(cipher_text = text, shift_amount = shift)
+
+
+
+    
